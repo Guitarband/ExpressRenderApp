5c00cfbd33d55f048ef1343e91480842832d4bf5
Guitarband <60247328+Guitarband@users.noreply.github.com>
2024-04-04 12:27:40 +1100

    Update app.js

--- app.js
+++ app.js
@@ -30,6 +30,10 @@ const html = `
 </html>
 `;
 
+let summonerPUUID = '';
+let username = '';
+let accountLevel = '';
+
 app.get("/", (req, res) => res.type('html').send(html));
 
 const server = app.listen(port, () => console.log(`Example app listening on port ${port}!`));
@@ -38,21 +42,49 @@ server.keepAliveTimeout = 120 * 1000;
 server.headersTimeout = 120 * 1000;
 console.log("Server is running");
 
-app.get('/summoner/:name', (req, res) => {
-  const summonerName = req.params.name;
-  const options = {
+const getPlayerCall = {
     hostname: 'oc1.api.riotgames.com',
     path: `/lol/summoner/v4/summoners/by-name/${summonerName}?api_key=${APIkey}`,
     method: 'GET'
   };
 
-  const request = https.request(options, (response) => {
+const getMasteryCall = {
+    hostname: 'oc1.api.riotgames.com',
+    path: `/lol/champion-mastery/v4/champion-masteries/by-puuid/${summonerPUUID}?api_key=${APIkey}`,
+    method: 'GET'
+  };
+
+app.get('/summoner/:name', (req, res) => {
+  const summonerName = req.params.name;
+  const userInfoRequest = https.request(getPlayerCall, (userInfoResponse) => {
+    let data = '';
+    userInfoResponse.on('data', (chunk) => {
+      data += chunk;
+    });
+    userInfoResponse.on('end', () => {
+      const summonerInfo = JSON.parse(data);
+      summonerPUUID = summonerInfo.puuid;
+      username = summonerInfo.name;
+      accountLevel = summonerInfo.summonerLevel;
+      res.json(summonerInfo);
+    });
+  });
+
+  userInfoRequest.on('error', (error) => {
+    console.error("Error occurred:", error.message);
+    res.status(500).json({ error: "Internal server error" });
+  });
+
+  const request = https.request(getPlayerCall, (response) => {
     let data = '';
     response.on('data', (chunk) => {
       data += chunk;
     });
     response.on('end', () => {
       const summonerInfo = JSON.parse(data);
+      summonerPUUID = summonerInfo.puuid;
+      username = summonerInfo.name;
+      accountLevel = summonerInfo.summonerLevel;
       res.json(summonerInfo);
     });
   });
